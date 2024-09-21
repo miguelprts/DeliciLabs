4f6fb4f777a48f0b869584673d01a91d5b8a89df
Miguel <miguelszportes@gmail.com>
2024-09-20 21:09:39 -0300

    conclusão da estrutura do projeto

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/rotasIndex.js');
+var rotasReceitas = require('./routes/rotasReceitas.js');
+var rotasApi = require('./routes/rotasApi.js');
 
 var app = express();
 
@@ -19,8 +21,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.json());
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/receitas', rotasReceitas);
+app.use('/receitas/api', rotasApi);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
